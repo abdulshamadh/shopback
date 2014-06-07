0a9011e8264e2d4fd18bb20f82ee8ced1e35c9fc
Abdul Shamadhu <abdulshamadhu@gmail.com>
2014-06-07 13:31:19 +0800

    New structure updated

--- application/views/defaultadmin/default/page_template.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$this->load->view('defaultadmin/default/subviews/header');
-$this->load->view('defaultadmin/default/subviews/sidebar',array('activemenu'=>$activemenu));
-$this->load->view($subview,array("contentdata"=>$contentdata));
-$this->load->view('defaultadmin/default/subviews/footer');
